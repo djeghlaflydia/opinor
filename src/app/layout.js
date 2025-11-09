15508a0921a33de370ee3d09a05567fdad3e29ee
DjeghlafLydia <djeghlaf.habiba@gmail.com>
2025-11-10 00:10:34 +0100

    .

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,19 +3,21 @@ import "./globals.css";
 
 const poppins = Poppins({
   subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"], // choisis les graisses que tu veux
-  variable: "--font-poppins",
+  weight: ["300", "400", "500", "600", "700"], 
 });
 
 export const metadata = {
   title: "Opinor",
   description: "Écoutez vos clients, Faites grandir votre réputation.",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body className={`${poppins.variable} font-sans antialiased`}>
+      <body className={`${poppins.className} antialiased`}>
         {children}
       </body>
     </html>
