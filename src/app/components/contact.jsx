46ddce18fc6df14b87e9db39d8785e694eeabbde
DjeghlafLydia <djeghlaf.habiba@gmail.com>
2025-11-10 15:14:58 +0100

    done.

--- src/app/components/contact.jsx
+++ src/app/components/contact.jsx
@@ -3,51 +3,50 @@ import { Phone, Mail, Instagram, User, MessageSquare } from "lucide-react";
 
 const Contact = () => {
   return (
-    <div className="bg-gradient-to-b from-[#FFFFFF] to-[#038788]/60 flex flex-col lg:flex-row items-center justify-center px-12 lg:px-32 py-20 gap-16">
-      <div className="w-full lg:w-[80%] bg-white py-10 px-12 rounded-xl shadow-md relative">
-        <h1 className="text-4xl font-semibold text-[#012222]">
+    <div className="bg-gradient-to-b from-white to-[#038788]/60 flex flex-col items-center justify-center px-4 md:px-12 lg:px-32 py-12 lg:py-20 gap-12">
+      <div className="w-full lg:w-[90%] bg-white py-8 md:py-10 px-4 md:px-12 rounded-xl shadow-md relative">
+        <h1 className="text-3xl md:text-4xl font-semibold text-[#012222]">
           Restons en contact !
         </h1>
-
-        <p className="text-[#012222] mt-2 leading-relaxed">
+        <p className="text-[#012222] mt-2 text-sm md:text-base leading-relaxed">
           Remplissez le formulaire ci-dessous et nous vous contacterons rapidement.
         </p>
 
-        <div className="flex mt-8 gap-10 relative">
+        <div className="flex flex-col lg:flex-row mt-8 gap-8">
           {/* Coordonnées */}
-          <div className="flex flex-col justify-between text-[#012222] text-lg w-full lg:w-1/2 relative">
-            <div className="flex flex-col gap-6 mt-2">
+          <div className="lg:flex flex-col hidden justify-between text-[#012222] text-sm md:text-base w-full lg:w-1/2 gap-8">
+            <div className="flex flex-col gap-6 mt-4">
               <div className="flex items-center gap-3">
-                <Phone className="w-6 h-6 text-[#038788]" />
+                <Phone className="w-5 h-5 md:w-6 md:h-6 text-[#038788]" />
                 <span>+213 774 338 408</span>
               </div>
               <div className="flex items-center gap-3">
-                <Mail className="w-6 h-6 text-[#038788]" />
+                <Mail className="w-5 h-5 md:w-6 md:h-6 text-[#038788]" />
                 <span>hello.opinor@workmail.com</span>
               </div>
             </div>
 
-            {/* Réseaux sociaux - tout en bas */}
-            <div className="flex-col items-center gap-5 mt-10 lg:mt-auto">
-              <h1 className="font-semibold mb-4 text-2xl text-[#012222]">
+            {/* Réseaux sociaux */}
+            <div className="flex flex-col items-center lg:items-start gap-4 mt-6 lg:mt-auto">
+              <h1 className="font-semibold mb-2 text-xl md:text-2xl text-[#012222]">
                 Connectez-vous avec nous
               </h1>
-              <div className="flex gap-2">
+              <div className="flex gap-3">
                 <a
                   href="https://www.instagram.com/"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="text-black hover:text-[#038788] transition"
                 >
-                  <Instagram className="w-10 h-9 p-1.5 rounded-full bg-[#E5F4F2]" />
+                  <Instagram className="w-8 h-8 md:w-10 md:h-10 p-1.5 rounded-full bg-[#E5F4F2]" />
                 </a>
                 <a
                   href="https://www.linkedin.com/"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="text-black w-10 h-9 flex justify-center items-center bg-[#E5F4F2] rounded-full hover:text-[#026C6C] transition"
+                  className="text-black w-8 h-8 md:w-10 md:h-10 flex justify-center items-center bg-[#E5F4F2] rounded-full hover:text-[#026C6C] transition"
                 >
-                  <p className="font-bold text-2xl">in</p>
+                  <p className="font-bold text-[0.8rem] md:text-xl">in</p>
                 </a>
               </div>
             </div>
@@ -56,7 +55,7 @@ const Contact = () => {
           {/* Formulaire */}
           <div className="w-full lg:w-1/2">
             <form className="flex flex-col gap-4">
-              {/* Champ Nom */}
+              {/* Nom */}
               <div className="flex items-center border border-gray-300 rounded-lg focus-within:ring-2 focus-within:ring-[#038788]">
                 <div className="flex items-center px-3 text-[#038788]">
                   <User className="w-5 h-5" />
@@ -65,11 +64,11 @@ const Contact = () => {
                 <input
                   type="text"
                   placeholder="Votre nom"
-                  className="flex-1 px-2 py-3 focus:outline-none"
+                  className="flex-1 px-2 py-3 focus:outline-none text-sm md:text-base"
                 />
               </div>
 
-              {/* Champ Email */}
+              {/* Email */}
               <div className="flex items-center border border-gray-300 rounded-lg focus-within:ring-2 focus-within:ring-[#038788]">
                 <div className="flex items-center px-3 text-[#038788]">
                   <Mail className="w-5 h-5" />
@@ -78,11 +77,11 @@ const Contact = () => {
                 <input
                   type="email"
                   placeholder="Votre email"
-                  className="flex-1 px-2 py-3 focus:outline-none"
+                  className="flex-1 px-2 py-3 focus:outline-none text-sm md:text-base"
                 />
               </div>
 
-              {/* Champ Message */}
+              {/* Message */}
               <div className="flex items-start border border-gray-300 rounded-lg focus-within:ring-2 focus-within:ring-[#038788]">
                 <div className="flex items-center px-3 pt-3 text-[#038788]">
                   <MessageSquare className="w-5 h-5" />
@@ -91,20 +90,59 @@ const Contact = () => {
                 <textarea
                   placeholder="Comment puis-je t'aider ?"
                   rows="3"
-                  className="flex-1 px-2 py-2 focus:outline-none resize-none"
+                  className="flex-1 px-2 py-2 md:py-3 focus:outline-none resize-none text-sm md:text-base"
                 ></textarea>
               </div>
 
-              <div className="flex justify-center lg:justify-start">
+              <div className="flex justify-center lg:justify-start mt-2">
                 <button
                   type="submit"
-                  className="bg-[#038788] text-white px-8 py-3 rounded-3xl hover:bg-[#026C6C] transition"
+                  className="bg-[#038788] text-white px-6 md:px-8 py-2 md:py-3 rounded-3xl hover:bg-[#026C6C] transition text-sm md:text-base"
                 >
                   Envoyer
                 </button>
               </div>
             </form>
           </div>
+
+          <div className="flex lg:hidden flex-col justify-between text-[#012222] text-sm md:text-base w-full lg:w-1/2 gap-8">
+            <div className="flex flex-col gap-4 mt-4">
+              <div className="flex items-center justify-center text-center gap-3">
+                <Phone className="w-5 h-5 md:w-6 md:h-6 text-[#038788]" />
+                <span>+213 774 338 408</span>
+              </div>
+              <div className="flex items-center justify-center text-center gap-3">
+                <Mail className="w-5 h-5 md:w-6 md:h-6 text-[#038788]" />
+                <span>hello.opinor@workmail.com</span>
+              </div>
+            </div>
+
+            {/* Réseaux sociaux */}
+            <div className="flex flex-col items-center lg:items-start gap-4 lg:mt-auto">
+              <h1 className="font-semibold mb-2 text-xl md:text-2xl text-[#012222]">
+                Connectez-vous avec nous
+              </h1>
+              <div className="flex gap-3">
+                <a
+                  href="https://www.instagram.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-black hover:text-[#038788] transition"
+                >
+                  <Instagram className="w-8 h-8 md:w-10 md:h-10 p-1.5 rounded-full bg-[#E5F4F2]" />
+                </a>
+                <a
+                  href="https://www.linkedin.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-black w-8 h-8 md:w-10 md:h-10 flex justify-center items-center bg-[#E5F4F2] rounded-full hover:text-[#026C6C] transition"
+                >
+                  <p className="font-bold text-[0.8rem] md:text-xl">in</p>
+                </a>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
